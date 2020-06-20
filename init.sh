05fb8810bc103ea49003f1b14fc19213152eb22c
daniel <daniel@nextplus-inc.com>
2020-06-20 15:23:16 +0800

    redmi

--- init.sh
+++ init.sh
@@ -10,12 +10,8 @@ mkdir -p $baseDir
 tee $absPath <<-'EOF'
 #!/bin/sh
 
-uci set network.lan.ipaddr='10.1.1.254'
-uci set network.lan.gateway='10.1.1.1'
-uci set network.lan.dns='1.2.4.8 210.2.4.8'
+uci set network.lan.ipaddr='192.168.123.1'
 uci commit network
-uci set wireless.default_radio0.disabled='1'
-uci commit wireless
 
 exit 0
 EOF
